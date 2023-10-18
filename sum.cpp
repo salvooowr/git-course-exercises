47789d4bcef53dfa09e91c3853ea3b02195dd5b4
salvatoreeee <salvoneri2010@hotmail.it>
2023-10-18 17:28:37 +0200

    feat: description

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+
+void main()
+{
+    int a, b, somma;
+    cout << "inserisci il primo numero";
+    cin >> a;
+    cout << "inserisci il secondo numero";
+    cin >> b;
+    somma = 0;
+    somma = a+b;
+    cout << "La somma è " << somma;
+}
